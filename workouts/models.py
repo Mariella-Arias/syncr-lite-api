93a7f874f5e99545b5332ad0fa3942b8f5e9c647
Mariella-Arias <ariasmariella.brm@gmail.com>
2025-01-28 13:42:51 -0600

    Create models: Block, BlockExercise, BlockExerciseData

--- workouts/models.py
+++ workouts/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-class Exercises(models.Model):
+class Exercise(models.Model):
     PARAM_CHOICES = [('reps', 'Repetitions'), ('duration', 'Duration')]
 
     label = models.CharField(max_length=50)
@@ -21,10 +21,32 @@ class Exercises(models.Model):
     def __str__(self):
         return self.value
 
-class Workouts(models.Model):
+class Workout(models.Model):
     name = models.CharField(max_length=100)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False, related_name="workouts")
 
     def __str__(self):
         return f"Template: {self.name} by {self.user}"
     
+class Block(models.Model):
+    workout = models.ForeignKey(Workout, on_delete=models.CASCADE, related_name="blocks")
+    position = models.IntegerField(null=False)
+
+    def __str__(self):
+        return f"Block {self.position} in {self.workout.name}"
+    
+class BlockExercise(models.Model):
+    block = models.ForeignKey(Block, on_delete=models.CASCADE, related_name="exercises")
+    exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
+    position = models.IntegerField(null=False)
+
+    def __str__(self):
+        return f"{self.exercise.value} in Block {self.block.position}, position {self.position}"
+    
+class BlockExerciseData(models.Model):
+    block_exercise = models.ForeignKey(BlockExercise, on_delete=models.CASCADE, related_name="data")
+    field = models.CharField(max_length=20)
+    value = models.IntegerField() # This will actually be either Integer or duration
+
+    def __str__(self):
+        return f"{self.field}: {self.value} for {self.block_exercise.exercise.value}"
