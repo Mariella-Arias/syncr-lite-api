6feb5021594f805439d6332cf973db5a2fd0aae3
Mariella-Arias <ariasmariella.brm@gmail.com>
2025-02-05 11:35:23 -0600

    Remove unused djoser settings

--- api_service/settings.py
+++ api_service/settings.py
@@ -178,7 +178,6 @@ DJOSER = {
     'SET_PASSWORD_RETYPE': True,
     'PASSWORD_RESET_CONFIRM_RETYPE': True,
     'PASSWORD_RESET_CONFIRM_URL': 'password/reset/confirm/{uid}/{token}',
-    'USERNAME_RESET_CONFIRM_URL': 'username/reset/confirm/{uid}/{token}',
     'ACTIVATION_URL': 'activate/{uid}/{token}',
     'SEND_ACTIVATION_EMAIL': True,
     'PASSWORD_RESET_SHOW_EMAIL_NOT_FOUND': True,
